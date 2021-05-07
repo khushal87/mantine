5f371f70beb14898cb2cea874c979781a2e53f7c
Khushal Agarwal <khushal.agarwal987@gmail.com>
2021-05-07 12:34:52 +0530

    [core,docs] add additional props to Hr component

--- src/mantine-core/src/Hr/Hr.tsx
+++ src/mantine-core/src/Hr/Hr.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import cx from 'clsx';
 import { useMantineTheme, DefaultProps, MantineNumberSize } from '@mantine/theme';
 import useStyles, { sizes } from './Hr.styles';
+import { Text } from '../Text/Text';
 
 export const HR_SIZES = sizes;
 
@@ -12,20 +13,58 @@ interface HrProps extends DefaultProps, React.ComponentPropsWithoutRef<'hr'> {
   /** Hr height */
   size?: MantineNumberSize;
 
+  /** Applies SubHeader Text to the Hr */
+  subHeader?: string;
+
+  /** Applies styles to the SubHeader Text */
+  subHeaderStyle?: React.CSSProperties;
+
   /** Hr color */
   color?: string;
+
+  /** Hr inset */
+  inset?: boolean;
+
+  /** Hr insetType. It applies inset to `left, right, middle`*/
+  insetType?: 'left' | 'right' | 'middle';
+
+  /** Applies orientation to the Hr */
+  orientation?: 'horizontal' | 'vertical';
 }
 
 export function Hr({
-  size = 'xs',
   className,
-  variant = 'dashed',
-  themeOverride,
   color = 'gray',
+  inset = false,
+  insetType = 'left',
+  orientation = 'horizontal',
+  size = 'xs',
+  subHeader,
+  subHeaderStyle,
+  themeOverride,
+  variant = 'solid',
   ...others
 }: HrProps) {
-  const classes = useStyles({ color, variant, size, theme: useMantineTheme(themeOverride) });
-  return <hr data-mantine-hr className={cx(classes.hr, className)} {...others} />;
+  const classes = useStyles({
+    color,
+    inset,
+    insetType,
+    size,
+    theme: useMantineTheme(themeOverride),
+    variant,
+    orientation,
+  });
+
+  if (orientation === 'vertical') {
+    return <div data-mantine-hr className={cx(classes.hrVertical, className)} {...others} />;
+  }
+  return (
+    <div data-mantine-hr className={cx(classes.hr, className)} {...others}>
+      <Text className={cx(classes.subHeader)} style={{ ...subHeaderStyle }}>
+        {subHeader}
+      </Text>
+    </div>
+  );
 }
 
 Hr.displayName = '@mantine/core/Hr';
